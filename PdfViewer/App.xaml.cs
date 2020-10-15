a8c4b48a76d9d8507d447c971149becf74d4c39e
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2020-10-15 15:47:03 +0600

    SyncFusion PDF viewer added in WPF

---
+++ PdfViewer/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PdfViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzM1NDI1QDMxMzgyZTMzMmUzMFV3SGorRGdKcFFkbUx6V1cxbG9VUXM3Y2NveEJhUllGOGRlWTFYSlBnY2s9");
+        }
+    }
+}
